684bb82196618dab3183f02878f6d03cec33d68e
funkychicken493 <82616867+funkychicken493@users.noreply.github.com>
2024-09-20 14:45:06 -0500

    improve wall jumping code
    Uses enums now and I refactored the names of some variables to meet the godot style guidelines

--- player.gd
+++ player.gd
@@ -11,6 +11,12 @@ class_name Player
 
 #region Wall Jumping
 
+enum WallDirection {
+	NONE,
+	LEFT,
+	RIGHT
+}
+
 @export var jump_from_wall_directional_velocity: float = 350.0
 
 @export var climbable_wall_layer: int = 2
@@ -23,18 +29,16 @@ class_name Player
 @export var jump_cut_strength: float = 0.5
 @export var jump_input_buffer_time: float = 0.125
 
-# 0 = no wall, 1 = left wall, 2 = right wall
-var sliding_on_wall: int = 0
+var sliding_on_wall: WallDirection = WallDirection.NONE
 
 var sliding_stamina: int = max_sliding_stamina
 
-var _wall_jumping: bool = false
-# 0 = no wall, 1 = left wall, 2 = right wall
-var _wall_jumping_from: int = 0
-var _leaving_wall: int = 0
+var wall_jumping: bool = false
+var wall_jumping_from: WallDirection = WallDirection.NONE
+var leaving_wall: WallDirection = WallDirection.NONE
 
 @warning_ignore("untyped_declaration") #reasoning: type of var is unknown until runtime
-var _last_wall_clinged_to = null
+var last_wall_clinged_to = null
 
 #endregion
 
@@ -58,43 +62,43 @@ var time_sliding: float = 0.0
 #endregion
 
 # Checks which side of the player is sliding on a wall.
-func sliding_on_wall_check(direction: float) -> int:
+func sliding_on_wall_check(direction: float) -> WallDirection:
 	var space := get_world_2d().direct_space_state
 	var left_query := PhysicsRayQueryParameters2D.create(global_position, global_position - Vector2(5, 0), climbable_wall_layer)
 	var left_intersect := space.intersect_ray(left_query)
 	if not left_intersect.is_empty():
 		# Reset stamina if we fall off one wall then cling onto another
-		if _last_wall_clinged_to != left_intersect["collider"]:
+		if last_wall_clinged_to != left_intersect["collider"]:
 			sliding_stamina = max_sliding_stamina
 		if direction < 0 and sliding_stamina > 0:
-			_last_wall_clinged_to = left_intersect["collider"]
-			return 1
+			last_wall_clinged_to = left_intersect["collider"]
+			return WallDirection.LEFT
 	var right_query := PhysicsRayQueryParameters2D.create(global_position, global_position + Vector2(5, 0), climbable_wall_layer)
 	var right_intersect := space.intersect_ray(right_query)
 	if not right_intersect.is_empty():
 		# Reset stamina if we fall off one wall then cling onto another
-		if _last_wall_clinged_to != right_intersect["collider"]:
+		if last_wall_clinged_to != right_intersect["collider"]:
 			sliding_stamina = max_sliding_stamina
 		if direction > 0 and sliding_stamina > 0:
-			_last_wall_clinged_to = right_intersect["collider"]
-			return 2
-	if sliding_on_wall != 0:
-		_leaving_wall = sliding_on_wall
+			last_wall_clinged_to = right_intersect["collider"]
+			return WallDirection.RIGHT
+	if sliding_on_wall != WallDirection.NONE:
+		leaving_wall = sliding_on_wall
 		var leaving_wall_timer := Timer.new()
 		add_child(leaving_wall_timer)
 		leaving_wall_timer.wait_time = leaving_wall_jump_grace_period
 		leaving_wall_timer.one_shot = true
 		leaving_wall_timer.timeout.connect(_reset_leaving_wall)
 		leaving_wall_timer.start()
-	return 0
+	return WallDirection.NONE
 
 # Resets the player's wall jumping variables.
 func _reset_wall_jumping() -> void:
-	_wall_jumping = false
-	_wall_jumping_from = 0
+	wall_jumping = false
+	wall_jumping_from = WallDirection.NONE
 	
 func _reset_leaving_wall() -> void:
-	_leaving_wall = 0
+	leaving_wall = WallDirection.NONE
 	
 #Checks the jump with input buffering
 func _check_jump_input() -> bool:
@@ -157,7 +161,7 @@ func _physics_process(delta: float) -> void:
 
 	# Add the gravity.
 	if not is_on_floor():
-		if sliding_on_wall == 0:
+		if sliding_on_wall == WallDirection.NONE:
 			if Input.is_action_pressed("crouch"):
 				velocity += get_gravity() * 2 * delta
 			else:
@@ -173,18 +177,18 @@ func _physics_process(delta: float) -> void:
 		sliding_stamina = max_sliding_stamina
 		#Disables jump-cutting
 		has_jump_cut = false
-	elif sliding_on_wall != 0:
+	elif sliding_on_wall != WallDirection.NONE:
 		sliding_stamina -= 1
 
 	# Handle the movement/deceleration.
-	if direction and not _wall_jumping and crouch_state != CrouchState.SLIDING:
+	if direction and not wall_jumping and crouch_state != CrouchState.SLIDING:
 		velocity.x += direction * speed
 		velocity.x = clampf(velocity.x, -speed, speed)
 	elif direction and crouch_state == CrouchState.CROUCHING:
 		velocity.x += direction * speed * crouch_speed_multiplier
 		velocity.x = clampf(velocity.x, -speed, speed)
-	elif _wall_jumping_from != 0:
-		if _wall_jumping_from == 1:
+	elif wall_jumping_from != WallDirection.NONE:
+		if wall_jumping_from == WallDirection.LEFT:
 			velocity.x = move_toward(velocity.x, jump_from_wall_directional_velocity, speed)
 		else:
 			velocity.x = move_toward(velocity.x, -jump_from_wall_directional_velocity, speed)
@@ -196,7 +200,7 @@ func _physics_process(delta: float) -> void:
 		velocity.x += move_toward(velocity.x, 0, speed)
 		
 	# Handle jump.
-	if _check_jump_input() and not _wall_jumping:
+	if _check_jump_input() and not wall_jumping:
 		if is_on_floor():
 			#Resets the jump input buffer time if successful jump performed
 			time_since_last_jump_input = 0.0
@@ -205,20 +209,20 @@ func _physics_process(delta: float) -> void:
 			
 			velocity.y = jump_velocity
 		# If we are sliding on a wall
-		elif sliding_on_wall != 0 or _leaving_wall != 0:
-			if ((direction < 0 and sliding_on_wall == 1) or _leaving_wall == 1) or ((direction > 0 and sliding_on_wall == 2) or _leaving_wall == 2):
+		elif sliding_on_wall != WallDirection.NONE or leaving_wall != WallDirection.NONE:
+			if ((direction < 0 and sliding_on_wall == WallDirection.LEFT) or leaving_wall == WallDirection.LEFT) or ((direction > 0 and sliding_on_wall == WallDirection.RIGHT) or leaving_wall == WallDirection.RIGHT):
 				#Resets the jump input buffer time if successful jump performed
 				time_since_last_jump_input = 0.0
 				#Enables jump-cutting
 				has_jump_cut = true
 				
 				sliding_stamina = max_sliding_stamina
-				_wall_jumping = true
+				wall_jumping = true
 				velocity.y = jump_velocity * 0.75
-				if sliding_on_wall != 0:
-					_wall_jumping_from = sliding_on_wall
+				if sliding_on_wall != WallDirection.NONE:
+					wall_jumping_from = sliding_on_wall
 				else:
-					_wall_jumping_from = _leaving_wall
+					wall_jumping_from = leaving_wall
 				var reset_wall_jumping_timer := Timer.new()
 				add_child(reset_wall_jumping_timer)
 				reset_wall_jumping_timer.wait_time = 0.25
@@ -227,7 +231,7 @@ func _physics_process(delta: float) -> void:
 				reset_wall_jumping_timer.start()
 	
 	#Jump-cuts if the jump input is released during jump
-	if !Input.is_action_pressed("jump") and has_jump_cut and sliding_on_wall == 0 and velocity.y < 0 and not is_on_floor():
+	if !Input.is_action_pressed("jump") and has_jump_cut and sliding_on_wall == WallDirection.NONE and velocity.y < 0 and not is_on_floor():
 		velocity.y = velocity.y * jump_cut_strength
 		#Disables jump-cutting
 		has_jump_cut = false;

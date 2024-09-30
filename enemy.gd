783c08f4ff81f639e2378dd9daa6fbc69fc1be99
abby1332 <abby1332@gmail.com>
2024-09-30 13:55:41 -0500

    Ready for review

--- enemy.gd
+++ enemy.gd
@@ -17,52 +17,55 @@ const chase_distance: float = 100 #Distance where enemy will start chasing
 const stop_chase_distance: float = 150 #Distance to stop chasing
 
 func _physics_process(delta):
-	if !is_on_floor():
-		velocity.y += gravity * delta
-		velocity.x = 0
-	check_player_distance()
-	move(delta)
-	move_and_slide()
+    if !is_on_floor():
+        velocity.y += gravity * delta
+        velocity.x = 0
+    check_player_distance()
+    move(delta)
+    move_and_slide()
 
 #Determines if the enemy should be chasing, roaming, or dying
 func move(delta):
-	if !dead:
-		if !is_chase:
-			velocity += dir * speed * delta #wander if we arent chasing
-		elif is_chase and !taking_damage: #If the enemy is taking damage it wont be moving
-			var dir_to_player = position.direction_to(player.position) * speed
-			velocity.x = dir_to_player.x #Don't let him chase player through the air
-			dir.x = abs(velocity.x) / velocity.x #Makes character turn left or right
-		elif taking_damage:
-			var knockback_dir = position.direction_to(player.position) * knockback_force
-			velocity.x = knockback_dir.x
-		is_roaming = true
-	elif dead:
-		velocity.x = 0 #No sliding around plz
-		
+    if !dead:
+        if !is_chase:
+            velocity += dir * speed * delta #wander if we arent chasing
+        elif is_chase and !taking_damage: #If the enemy is taking damage it wont be moving
+            var dir_to_player = position.direction_to(player.position) * speed
+            velocity.x = dir_to_player.x #Don't let him chase player through the air
+            dir.x = abs(velocity.x) / velocity.x #Makes character turn left or right
+        elif taking_damage:
+            var knockback_dir = position.direction_to(player.position) * knockback_force
+            velocity.x = knockback_dir.x
+        is_roaming = true
+    elif dead:
+        velocity.x = 0 #No sliding around plz
+
+        
 #When the enemy is wandering, it will move back and forth at random
 func _on_direction_timer_timeout() -> void:
-	$DirectionTimer.wait_time = choose([1.5,2.0,2.5])
-	if !is_chase:
-		dir = choose([Vector2.RIGHT, Vector2.LEFT])
-		velocity.x = 0 #No sliding plz
+    $DirectionTimer.wait_time = choose([1.5,2.0,2.5])
+    if !is_chase:
+        dir = choose([Vector2.RIGHT, Vector2.LEFT])
+        velocity.x = 0 #No sliding plz
 
 #Choose a direction to wander in randomly
 func choose(array):
-	array.shuffle()
-	return array.front()
+    array.shuffle()
+    return array.front()
 
 #If player enterse the damage collision zone, player dies
 func _on_deal_damage_area_body_entered(body: Node2D) -> void:
-	if body.name == "Player":
-		body.die()
+    print("Someone entered")
+    if body.name == "Player":
+        print("Killing the player")
+        body.die()
 
 # Check the distance between enemy and player to determine chasing/roaming
 func check_player_distance():
-	if player and !dead: #Chase player when it gets too close
-		var distance_to_player = position.distance_to(player.position)
-		if distance_to_player < chase_distance:
-			is_chase = true
-		elif (distance_to_player > stop_chase_distance) or player.dead: #Start romaing when the player is dead or leaves area
-			is_chase = false
-			is_roaming = true
+    if player and !dead: #Chase player when it gets too close
+        var distance_to_player = position.distance_to(player.position)
+        if distance_to_player < chase_distance:
+            is_chase = true
+        elif (distance_to_player > stop_chase_distance) or player.dead: #Start romaing when the player is dead or leaves area
+            is_chase = false
+            is_roaming = true

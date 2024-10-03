e7608691f1edd88edbae099188b57ff612f5c6b6
funkychicken493 <82616867+funkychicken493@users.noreply.github.com>
2024-10-03 11:16:33 -0500

    Make card spawner use two separate scenes

--- card_spawner.gd
+++ card_spawner.gd
@@ -2,6 +2,7 @@ extends Node2D
 class_name CardSpawner
 
 @export var dropped_card_scene: PackedScene
+@export var card_scene: PackedScene
 
 var spawned_dropped_card: DroppedCard = null
 
@@ -10,6 +11,9 @@ func spawn() -> bool:
 		return false
 	spawned_dropped_card = dropped_card_scene.instantiate() as DroppedCard
 	add_child(spawned_dropped_card)
+	var spawned_card := card_scene.instantiate() as Card
+	spawned_dropped_card.add_child(spawned_card)
+	spawned_dropped_card.card = spawned_card
 	return true
 
 func _ready() -> void:

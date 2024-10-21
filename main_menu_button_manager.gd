4aafa46eac6e90233297a612535cdc76da396255
funkychicken493 <82616867+funkychicken493@users.noreply.github.com>
2024-10-21 18:38:01 -0500

    main menu scene

---
+++ main_menu_button_manager.gd
@@ -0,0 +1,16 @@
+extends Control
+
+func _on_start_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_option_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_credit_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_exit_button_pressed() -> void:
+	pass # Replace with function body.

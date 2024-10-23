dd92370a1074d2678b4e20f42f38557bbcdb2782
Funky <82616867+funkychicken493@users.noreply.github.com>
2024-10-23 18:43:06 -0500

    rotating title

--- main_menu_button_manager.gd
+++ main_menu_button_manager.gd
@@ -2,15 +2,29 @@ extends Control
 
 @export var first_level_path: String
 
+@export var credit_screen_parent: Control
+
 func _on_start_button_pressed() -> void:
-	print("Button!")
+	if(credit_screen_parent.visible):
+		return
 	RespawnManager.load_scene(first_level_path)
 
 func _on_option_button_pressed() -> void:
+	if(credit_screen_parent.visible):
+		return
 	pass # Replace with function body.
 
 func _on_credit_button_pressed() -> void:
-	pass # Replace with function body.
+	if(credit_screen_parent.visible):
+		return
+	credit_screen_parent.show()
+
+func _on_credit_exit_button_pressed() -> void:
+	if(!credit_screen_parent.visible):
+		return
+	credit_screen_parent.hide()
 
 func _on_exit_button_pressed() -> void:
+	if(credit_screen_parent.visible):
+		return
 	get_tree().quit()

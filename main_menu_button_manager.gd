f2b27a268f814f30763201b9f3a97eacdf85b38a
funkychicken493 <82616867+funkychicken493@users.noreply.github.com>
2024-10-25 14:54:03 -0500

    options done

--- main_menu_button_manager.gd
+++ main_menu_button_manager.gd
@@ -3,6 +3,7 @@ extends Control
 @export var first_level_path: String
 
 @export var credit_screen_parent: Control
+@export var options_screen_parent: Control
 
 func _on_start_button_pressed() -> void:
 	if(credit_screen_parent.visible):
@@ -10,13 +11,19 @@ func _on_start_button_pressed() -> void:
 	RespawnManager.load_scene(first_level_path)
 
 func _on_option_button_pressed() -> void:
-	if(credit_screen_parent.visible):
+	if(credit_screen_parent.visible || options_screen_parent.visible):
 		return
-	pass # Replace with function body.
+	options_screen_parent.show()
+
+func _on_options_exit_pressed() -> void:
+	if(credit_screen_parent.visible || !options_screen_parent.visible):
+		return
+	options_screen_parent.hide()
 
 func _on_credit_button_pressed() -> void:
 	if(credit_screen_parent.visible):
 		return
+	options_screen_parent.hide()
 	credit_screen_parent.show()
 
 func _on_credit_exit_button_pressed() -> void:
--- options_manager.gd
+++ options_manager.gd
@@ -1,5 +1,8 @@
 extends Node
 
+signal options_loaded(options: Dictionary)
+signal option_changed(section: String, key: String, value: Variant)
+
 const default_options: Dictionary = {
 	"audio.master" = 100,
 	"audio.music" = 100,
@@ -10,7 +13,7 @@ const default_options: Dictionary = {
 var options: Dictionary = default_options.duplicate(true)
 var config: ConfigFile = ConfigFile.new()
 
-func _enter_tree() -> void:
+func _ready() -> void:
 	var err := config.load("user://options.cfg")
 	if err != OK:
 		reset()
@@ -19,11 +22,13 @@ func _enter_tree() -> void:
 		for key in config.get_section_keys(section):
 			options[section + "." + key] = config.get_value(section, key)
 	save()
+	options_loaded.emit(options)
 
 func reset() -> void:
 	for key: String in default_options.keys():
 		options[key] = default_options[key]
 	save()
+	options_loaded.emit(options)
 
 func save() -> void:
 	for key: String in options.keys():
@@ -36,8 +41,15 @@ func get_option(section: String, key: String) -> Variant:
 		options[section + "." + key] = default_options[section + "." + key]
 	return options[section + "." + key]
 	
+func set_option_without_saving(section: String, key: String, value: Variant) -> void:
+	if get_option(section, key) == value:
+		return
+	options[section + "." + key] = value
+	option_changed.emit(section, key, value)
+	
 func set_option(section: String, key: String, value: Variant) -> void:
 	if get_option(section, key) == value:
 		return
 	options[section + "." + key] = value
 	save()
+	option_changed.emit(section, key, value)

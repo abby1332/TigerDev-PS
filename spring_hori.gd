8ef7e0f561908053d5b01fa9edd20e941b20dff7
Floofiey <dolihitej@hotmail.com>
2024-11-03 03:36:35 -0600

    Horizontal decent but needs input lock

--- spring_hori.gd
+++ spring_hori.gd
@@ -5,22 +5,48 @@ func _on_body_entered(body: Node2D) -> void:
 	if not body is Player:
 		return
 	var player := body as Player
+	var stime := Timer.new()
+	player.set_process_input(false)
+	add_child(stime)
+	stime.one_shot = true
+	stime.autostart = false
+	stime.wait_time = 1.2
+	stime.timeout.connect(func(): player.set_process_input(true))
+	var rebound_velocity : int
 	if player.velocity.x > 0:
 		if player.velocity.x > 600:
 			if player.velocity.x > 1000:
 				player.velocity.x *= -1
+				stime.start()
+				rebound_velocity = player.velocity.x
+				player.velocity.x = lerp(rebound_velocity, rebound_velocity - 400, stime.time_left)
 			else:
 				player.velocity.x *= -1
 				player.velocity.x -= 100
+				stime.start()
+				rebound_velocity = player.velocity.x
+				player.velocity.x = lerp(rebound_velocity, rebound_velocity - 400, stime.time_left)
 		else:
 			player.velocity.x = -600
+			stime.start()
+			rebound_velocity = player.velocity.x
+			player.velocity.x = lerp(rebound_velocity, rebound_velocity - 400, stime.time_left)
 	else :
 		if player.velocity.x < -600:
 			if player.velocity.x < -1000:
 				player.velocity.x *= -1
+				stime.start()
+				rebound_velocity = player.velocity.x
+				player.velocity.x = lerp(rebound_velocity, rebound_velocity + 400, stime.time_left)
 			else:
 				player.velocity.x *= -1
 				player.velocity.x += 100
+				stime.start()
+				rebound_velocity = player.velocity.x
+				player.velocity.x = lerp(rebound_velocity, rebound_velocity + 400, stime.time_left)
 		else:
 			player.velocity.x = 600
+			stime.start()
+			rebound_velocity = player.velocity.x
+			player.velocity.x = lerp(rebound_velocity, rebound_velocity + 400, stime.time_left)
 	

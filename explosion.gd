97d5f4457c467521ac5241bb1a35f2b309df5e09
Funky <82616867+funkychicken493@users.noreply.github.com>
2024-11-16 12:15:07 -0600

    add sound

--- explosion.gd
+++ explosion.gd
@@ -1,7 +1,8 @@
 extends Area2D
 class_name Explosion
 
-@onready var particles = $Particles
+@onready var particles: GPUParticles2D = $Particles
+@onready var sound: AudioStreamPlayer2D = $Sound
 
 var hurts_player: bool = true
 var size: float = 1.0
@@ -9,6 +10,8 @@ var size: float = 1.0
 signal finished
 
 func start() -> void:
+	sound.pitch_scale += randf_range(-0.4, 0)
+	sound.play()
 	scale = Vector2(size, size)
 	particles.emitting = true
 	for body in get_overlapping_bodies():

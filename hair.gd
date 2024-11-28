8a8363f9c668d11812ccbca906fc47856b01f4a3
ASlightlyOvergrownCactus <ASlightlyOvergrownCactus@gmail.com>
2024-11-28 17:32:42 -0600

    Minor Commit before its all over
    If anyone else has a better idea please let me know but I think im going to have to code offsets for each frame of animation and oml...

--- hair.gd
+++ hair.gd
@@ -1,15 +1,17 @@
 extends Node2D
 # TODO: Change global hair node position based on current animation? Or even follow certain animations
-@onready var sprite_count = 16.0
+@onready var sprite_count = 8.0
 @onready var grav_scale = 8.0 # Higher it is the lower gravity is :p
 @onready var distance = 0.1
 @onready var radius = 2.5
+var animation_offset := Vector2.ZERO
 var points = []
 
 func _ready() -> void:
 	# max_dist, (pos_x, pos_y), radius
 	for i in range(sprite_count):
 		points.append([distance - i * (distance / sprite_count), Vector2(0, 0), radius - i * (radius / sprite_count)])
+		
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -27,7 +29,9 @@ func _process(delta: float) -> void:
 				points[i][1] = offset + points[i-1][1]
 			points[i][1] += ((owner.get_gravity() / (grav_scale * 20)) * delta)
 			queue_redraw()
+		
 	
 func _draw():
+	pass
 	for point in points:
-		draw_circle(point[1] - global_position, point[2], Color.BROWN)
+		draw_circle(point[1] - global_position, point[2], Color(0.6745, 0.196, 0.196, 1.0))

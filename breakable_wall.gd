1ca91beeec6ee2be1c7f7d03646a934a1b35c58d
abby1332 <abby1332@gmail.com>
2024-11-29 10:41:36 -0500

    Now works with stomp card and when player is already next to wall. Does not trigger wall chain break. Am lazy.

--- breakable_wall.gd
+++ breakable_wall.gd
@@ -1,15 +1,26 @@
 extends StaticBody2D
+var bodies_inside: Array = [] #Used to parse over the player
+signal wall_broken(position: Vector2)
+var is_breaking: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	connect("wall_broken", Callable(self, "_on_neighbor_wall_broken"))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	#Continuously check if the player is in kill mode while inside area
+	check_break_condition()
 
 func break_wall(player_position: Vector2):
+	#Prevent infinite recursion
+	if is_breaking:
+		return
+	
+	is_breaking = true
+	#Emit a signal to notify neighboring walls
+	emit_signal("wall_broken", global_position)
+	
 	var particles = $WallBreakParticles
 
    # Calculate the direction opposite to the player's entry
@@ -29,5 +40,26 @@ func break_wall(player_position: Vector2):
 	
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body.kill_everything_mode:
-		break_wall(body.global_position)
+	if body is Player:
+		bodies_inside.append(body)
+		#Start checking for condition
+		set_process(true)
+		check_break_condition()
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	bodies_inside.erase(body)
+	#Stop checking
+	set_process(false)
+	
+#Keep track of the kill mode
+func check_break_condition():
+	for body in bodies_inside:
+		if body.kill_everything_mode or body.is_stomping:
+			print("We break now")
+			break_wall(body.global_position)
+
+#Break nearby walls
+func _on_neighbor_wall_broken(broken_position: Vector2):
+	if (global_position - broken_position).length() < 100: #Adjust as needed
+		break_wall(broken_position)

0f5b9c5b502d28bbd82452a4f7072de90b7ff69e
ASlightlyOvergrownCactus <ASlightlyOvergrownCactus@gmail.com>
2024-12-01 14:36:31 -0600

    Partially Done

--- hair.gd
+++ hair.gd
@@ -1,7 +1,7 @@
 extends Node2D
 # TODO: Change global hair node position based on current animation? Or even follow certain animations
-@onready var sprite_count = 8.0
-@onready var grav_scale = 8.0 # Higher it is the lower gravity is :p
+@onready var sprite_count = 4.0
+@onready var grav_scale = 3.0 # Higher it is the lower gravity is :p
 @onready var distance = 0.1
 @onready var radius = 2.5
 var animation_offset := Vector2.ZERO
@@ -10,16 +10,79 @@ var points = []
 func _ready() -> void:
 	# max_dist, (pos_x, pos_y), radius
 	for i in range(sprite_count):
-		points.append([distance - i * (distance / sprite_count), Vector2(0, 0), radius - i * (radius / sprite_count)])
-		
+		points.append([1, Vector2(0, 0), 2])
+	animation_offset = Vector2(1.2, -10) # If the player starting direction ever changes, change the x value here to negative
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	visible = !(owner.dead)
+	# Animation offset (evil)
+	var animation_machine = owner.animation_machine
+
+	var direction = owner.last_look_direction
+	var running = animation_machine.sprite.animation == animation_machine.rl("run")
+	var left = false
+	
+	if (direction.x < 0.0):
+		left = true
+	elif (direction.x > 0.0):
+		left = false
+	
+	if running and not left: # Weird offset here due to animation not being centered im like 80% sure
+		animation_offset.x = -0.6
+	else:
+		animation_offset.x = 1.2
+		
+	if (direction.x < 0.0):
+		animation_offset.x = abs(animation_offset.x)
+	elif (direction.x > 0.0):
+		animation_offset.x = abs(animation_offset.x) * -1.0
+		
+	if (animation_machine.sprite.animation == animation_machine.rl("idle")): # Hair follows head bob here
+		var frame = animation_machine.sprite.frame
+		match frame:
+			0:
+				animation_offset.y = -10
+			1:
+				animation_offset.y = -9.5
+			2:
+				animation_offset.y = -9
+			3:
+				animation_offset.y = -9.5
+			4:
+				animation_offset.y = -10
+			5:
+				animation_offset.y = -10.5
+	elif (animation_machine.sprite.animation == animation_machine.rl("land")): # This is awful
+		var frame = animation_machine.sprite.frame
+		match frame:
+			0:
+				animation_offset.y = -6
+			1:
+				animation_offset.y = -7
+			2:
+				animation_offset.y = -7.5
+			3:
+				animation_offset.y = -8
+			4:
+				animation_offset.y = -8.5
+			5:
+				animation_offset.y = -8.5
+			6:
+				animation_offset.y = -9
+			7:
+				animation_offset.y = -9.5
+			8:
+				animation_offset.y = -10
+	else:
+		animation_offset.y = -10
+		
+	
+	
 	for j in range(sprite_count):
 		for i in range(len(points)-1, -1, -1):
 			if i == 0:
-				points[i][1] = global_position
+				points[i][1] = global_position + animation_offset
 				continue
 			
 			if points[i][1].distance_to(points[i-1][1]) > points[i][2]:
@@ -32,6 +95,5 @@ func _process(delta: float) -> void:
 		
 	
 func _draw():
-	pass
-	for point in points:
+	for point in points: # replace with actual proper sprites later?
 		draw_circle(point[1] - global_position, point[2], Color(0.6745, 0.196, 0.196, 1.0))

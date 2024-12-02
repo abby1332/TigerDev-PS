1f5ae8a9d669850b68a2f23a091018e59614e203
abby1332 <abby1332@gmail.com>
2024-12-02 15:35:52 -0600

    Trying to add remaining art and sfx

--- resources/all spring stuff/spring_vertical.gd
+++ resources/all spring stuff/spring_vertical.gd
@@ -1,10 +1,17 @@
 extends Area2D
 class_name spring_vert
-
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var sfx: AudioStreamPlayer2D = $AudioStreamPlayer2D
+	
 func _on_body_entered(body: Node2D) -> void:
 	if not body is Player:
 		return
 	var player := body as Player
+	
+	#Trigger bounce animation
+	sprite.play("boing")
+	sfx.play()
+	
 	if player.velocity.y > 0:
 		if player.velocity.y > 600:
 			if player.velocity.y > 1000:

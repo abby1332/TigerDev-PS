adda89afdf0ef2ad8cae0c8b793bdc4785237c3c
abby1332 <abby1332@gmail.com>
2025-02-02 10:49:30 -0600

    Moved around some of the audio files and made the player manager dynamic. Crashes everytime I open project settings wtf

--- content/player/player_sound_machine.gd
+++ content/player/player_sound_machine.gd
@@ -2,6 +2,8 @@ extends Node
 class_name PlayerSoundMachine
 ## Provides easy access to audio streams that are connected to the player
 
+@export var mute: bool = false
+
 static var main: PlayerSoundMachine
 
 var sound_players: Dictionary = {}
@@ -10,10 +12,13 @@ var sound_players: Dictionary = {}
 func _ready() -> void:
 	main = self
 	for child in get_children():
-		if not child is AudioStreamPlayer2D:
-			continue
-		sound_players[child.name] = child as AudioStreamPlayer2D
+		if child is AudioStreamPlayer2D:
+			sound_players[child.name] = child as AudioStreamPlayer2D
 
-
-func stream(name: String) -> AudioStreamPlayer2D:
-	return sound_players[name]
+# name = sound effect name you want to play
+# Call this function inside of the player action to trigger the sound effect
+func play_sound(name: String) -> void:
+	if sound_players.has(name):
+		sound_players[name].play()
+	else:
+		print_debug("Sound '" + name + "' not found in player sound manager")

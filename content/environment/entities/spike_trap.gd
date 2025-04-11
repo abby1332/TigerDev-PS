5b8933db29eb1429ff5b2d1df82a787c3a1e5a70
EMcConnell20 <ethan.mcconnell20@gmail.com>
2025-04-11 13:29:00 -0700

    Finished Porting Player Sprite

--- content/environment/entities/spike_trap.gd
+++ content/environment/entities/spike_trap.gd
@@ -1,9 +1,11 @@
-extends Area2D
-class_name SpikeTrap
 ## Spike Trap that causes bodily harm to the player
 ## https://medlineplus.gov/ency/article/000043.htm
+class_name SpikeTrap
+extends Area2D
 
 
+# TODO - Get rid of this.
+# Reason - Because this has no reason to be  an entire class?? Just have the player check if they're colliding with spikes???
 ## When the player enters the spike trap kill them instantly
 func _on_body_entered(body: Node2D) -> void:
 	if not body is Player:
--- content/player/player_character.gd
+++ content/player/player_character.gd
@@ -20,5 +20,32 @@ enum MoveState {
 @export var spawn_point: SpawnPoint;
 
 @export_category("Player Scene Items")
-@export var card_manager: CameraManager;
-@export var player_sprite: PlayerSprite2D;
+@export var cards: CardManager;
+@export var sprite: PlayerSprite2D;
+@export var camera: Camera2D;
+@export var head_raycast: RayCast2D;
+@export var leg_raycast: RayCast2D;
+
+#endregion
+
+#region Movement Vars
+
+## Defines the current state of and restricions on the player's movements.
+var move_state: MoveState = MoveState.Falling;
+
+## Stores the relevant direction & magnitude of a move state.
+var move_direction: Vector2 = Vector2.ZERO;
+
+## Time spent in current movement state. (s)
+var hang_time: float = 0.0;
+
+## True when player can jump.
+var can_jump: bool = false;
+
+## True when player can grasp a wall.
+var can_grasp: bool = false;
+
+## True when player can launch from a wall.
+var can_launch: bool = false;
+
+#endregion
